d9a9a81b9e4b6f36dbfad1c6206f36720f31cd36
xuechen <me@ononoki.org>
2024-06-21 12:24:41 +0800

    Remove userid module

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_browser_module --without-http_empty_gif_module --without-http_limit_conn_module \
 --without-http_memcached_module --without-http_mirror_module \
 --without-http_referer_module --without-http_split_clients_module \
---without-http_scgi_module --without-http_ssi_module \
+--without-http_scgi_module --without-http_ssi_module --without-http_userid_module \
 --without-http_upstream_hash_module --without-http_upstream_ip_hash_module \
 --without-http_upstream_keepalive_module --without-http_upstream_least_conn_module \
 --without-http_upstream_random_module --without-http_upstream_zone_module \
